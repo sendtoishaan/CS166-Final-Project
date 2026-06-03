6a2c70c67a23964d414488d0e21875566b4921b0
sendtoishaan <sendtoishaan@gmail.com>
2026-06-03 13:54:51 -0700

    Fixed and Implemented Main UI using Streamlit library

--- DATABASE_CONNECTION.py
+++ DATABASE_CONNECTION.py
@@ -5,8 +5,8 @@ from psycopg2.extras import RealDictCursor
 DB_CONFIG = {
     "host":     os.environ.get("DB_HOST",     "localhost"),
     "port":     int(os.environ.get("DB_PORT", 5432)),
-    "dbname":   os.environ.get("DB_NAME",     "auction_db"),
-    "user":     os.environ.get("DB_USER",     "postgres"),
+    "dbname":   os.environ.get("DB_NAME",     "CS166_AUCTION_AND_BIDDING_DATABASE"),
+    "user":     os.environ.get("DB_USER",     "ishaanvenkat"),
     "password": os.environ.get("DB_PASSWORD", ""),
 }
  
